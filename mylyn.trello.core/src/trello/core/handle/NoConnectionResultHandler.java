3fa66d9e919be36424458807ada502aab2be1184
дом <@169.254.170.118>
2020-03-01 19:22:01 +0300

    Интерфейс NoConnectionResultHandler переделан в абстрактный класс.

--- mylyn.trello.core/src/trello/core/handle/NoConnectionResultHandler.java
+++ mylyn.trello.core/src/trello/core/handle/NoConnectionResultHandler.java
@@ -1,17 +1,31 @@
 package trello.core.handle;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+
 /**
- * Интерфейс обработчика результата попытки установки соединения с trello.com
- * (при отсутствии подключения)
- *
+ * Абстрактный класс обработчика результата попытки установки соединения с trello.com
+ * (при отсутствии подключения).
  */
-public interface NoConnectionResultHandler
+public abstract class NoConnectionResultHandler
 {
+	private static Display display;
+	private static Shell shell;
+	
 	/**
 	 * Метод для создания окна с сообщением пользователю.
-	 * 
-	 * @param a_message
-	 *            - сообщение
+	 * @param a_message - сообщение
 	 */
-	void createMessage(String a_message);
+	public static void createMessage(String a_message)
+	{
+		display = new Display();
+		shell = new Shell(display);
+		MessageBox messageBox = new MessageBox(shell, SWT.ERROR);
+		messageBox.setText("Error");
+		messageBox.setMessage(a_message);
+		messageBox.open();
+		display.dispose();
+	}
 }
