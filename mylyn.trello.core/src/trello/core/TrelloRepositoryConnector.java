0a17943d9dbf493a1c63af5acf98375cbec514b9
дом <@169.254.170.118>
2020-03-09 23:48:54 +0300

    Теперь в классе TrelloRepositoryConnector метод "getLabel" возвращает константу REPOSITORY_LABEL, в методе "getTaskIdOrUrl" используются методы клиента для возврата карты по её id или url, метод "getTaskData" использует обработчик TrelloTaskDataHandler для возврата данных задачи; написано начало реализации метода "performQuery".

--- mylyn.trello.core/src/trello/core/TrelloRepositoryConnector.java
+++ mylyn.trello.core/src/trello/core/TrelloRepositoryConnector.java
@@ -10,15 +10,14 @@ import org.eclipse.mylyn.tasks.core.AbstractRepositoryConnector;
 import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
 import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
 import org.eclipse.mylyn.tasks.core.data.TaskAttributeMapper;
 import org.eclipse.mylyn.tasks.core.data.TaskData;
 import org.eclipse.mylyn.tasks.core.data.TaskDataCollector;
 import org.eclipse.mylyn.tasks.core.sync.ISynchronizationSession;
+import trello.core.connection.ITrelloConnection;
+import trello.core.connection.TrelloConnection;
 import trello.core.connection.TrelloConnectionImit;
-import trello.core.handle.INoConnectionResultHandler;
-import trello.core.handle.NoConnectionResultHandler;
-import trello.core.model.Board;
-import trello.core.model.BoardList;
 import trello.core.model.Card;
 import trello.core.model.CardList;
 
@@ -27,6 +26,7 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 	public final static String CONNECTOR_KIND = "trello";
 	public final static String REPOSITORY_URL = "https://trello.com";
 	public final static String REPOSITORY_LABEL = "Trello.com";
+	private final TrelloTaskDataHandler taskDataHandler = new TrelloTaskDataHandler();
 
 	@Override
 	public boolean canCreateNewTask(@NonNull TaskRepository a_repository)
@@ -49,7 +49,7 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 	@Override
 	public @NonNull String getLabel()
 	{
-		return "Trello";
+		return REPOSITORY_LABEL;
 	}
 
 	@Override
@@ -74,36 +74,17 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 
 	private String getTaskIdOrUrl (boolean a_isId, String a_idOrUrl)
 	{
-		INoConnectionResultHandler handler = new NoConnectionResultHandler();
-		TrelloConnectionImit connection = new TrelloConnectionImit("1c8962f69eea55f6346aacabf4b9d90e", "f51cede3da475d589f56dc510ce4292bd4bcae622d1cca92e75821309cef697f");//Тестовая реализация
+		TrelloConnection client = new TrelloConnection(ITrelloConnection.DEFAULT_KEY, ITrelloConnection.DEFAULT_TOKEN);
 		try
 		{
-			BoardList boardList = connection.getBoardList();
-			for (Board b : boardList.getBoards())
-			{
-				for (CardList l : b.getCardLists())
-				{
-					for (Card c : l.getCards())
-					{
-						if (a_isId)
-						{
-							if (c.getUrl().equals(a_idOrUrl))
-							{
-								return c.getId();
-							}
-						}
-						else
-						if (c.getId().equals(a_idOrUrl))
-						{
-							return c.getUrl();
-						}
-					}
-				}
-			}
+			if (a_isId)
+				return client.getCardByUrl(a_idOrUrl).getId();
+			else
+				return client.getCardById(a_idOrUrl).getUrl();
 		}
 		catch(Exception e)
 		{
-			handler.createMessage(e.getMessage());
+			e.printStackTrace();
 		}
 		return null;
 	}
@@ -117,21 +98,47 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 	@Override
 	public void updateTaskFromTaskData(@NonNull TaskRepository a_taskRepository, @NonNull ITask a_task, @NonNull TaskData a_taskData)
 	{
+		
 	}
 
 	@Override
 	public @NonNull TaskData getTaskData(@NonNull TaskRepository a_repository, @NonNull String a_taskId, 
 			                             IProgressMonitor a_monitor) throws CoreException
 	{
-		TaskData taskData = new TaskData(new TaskAttributeMapper(a_repository), a_repository.getConnectorKind(), a_repository.getRepositoryUrl(), a_taskId); 
-		return taskData; 
+		return taskDataHandler.getTaskData(a_repository, a_taskId, a_monitor);
 	}
 
 	@Override
 	public IStatus performQuery(@NonNull TaskRepository a_repository, @NonNull IRepositoryQuery a_query, @NonNull TaskDataCollector a_collector, 
 			                    @Nullable ISynchronizationSession a_session, IProgressMonitor a_monitor)
 	{
-		return Status.OK_STATUS; 
+		try 
+		{////////////////Validate Settings!!!!!!!!!!!!!!
+			a_monitor.beginTask("Main task", IProgressMonitor.UNKNOWN);
+			TrelloConnectionImit client = new TrelloConnectionImit(null, null);
+			for (CardList l : client.getCardLists("5e401943376hk65477b36237"))
+			{
+				for (Card c : l.getCards())
+				{
+					TaskData taskData = new TaskData(new TaskAttributeMapper(a_repository), a_repository.getConnectorKind(), a_repository.getRepositoryUrl(), c.getId());
+					taskData.setPartial(true);
+					
+					TaskAttribute attribute = new TaskAttributeMapper(a_repository).createTaskAttachment(taskData);
+					attribute.setValue(c.getId() + ""); //$NON-NLS-1$
+					a_collector.accept(taskData);
+				}
+			}
+			return Status.OK_STATUS;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return Status.CANCEL_STATUS;
+		}
+		finally
+		{
+			a_monitor.done();
+		}
 	}
 
 	@Override
