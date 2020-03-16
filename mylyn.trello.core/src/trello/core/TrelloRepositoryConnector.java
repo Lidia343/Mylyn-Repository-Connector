1f098ceab7ae0a8361da1e6793d5700ea2df27c0
дом <@169.254.170.118>
2020-03-16 16:22:01 +0300

    Создана строковая константа, обозначающая дату обновления ключа задачи. Возвращаемые методами "canCreateNewTask", "canCreateTaskFromKey" и "canSynchronizeTask" значения изменены на "true". Метод "getTaskDataHandler" возвращает поле класса - обработчик данных задачи. Переопределены методы "hasTaskChanged", "updateTaskFromTaskData". Добавлена реализация методов "preSynchronization", "postSynchronization", "canDeleteTask", "deleteTask" и "canGetTaskHistory".

--- mylyn.trello.core/src/trello/core/TrelloRepositoryConnector.java
+++ mylyn.trello.core/src/trello/core/TrelloRepositoryConnector.java
@@ -1,11 +1,16 @@
 package trello.core;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.Set;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.mylyn.commons.net.Policy;
 import org.eclipse.mylyn.tasks.core.AbstractRepositoryConnector;
 import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
 import org.eclipse.mylyn.tasks.core.ITask;
@@ -14,6 +19,7 @@ import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
 import org.eclipse.mylyn.tasks.core.data.TaskAttributeMapper;
 import org.eclipse.mylyn.tasks.core.data.TaskData;
 import org.eclipse.mylyn.tasks.core.data.TaskDataCollector;
+import org.eclipse.mylyn.tasks.core.data.TaskMapper;
 import org.eclipse.mylyn.tasks.core.sync.ISynchronizationSession;
 import trello.core.connection.ITrelloConnection;
 import trello.core.connection.TrelloConnection;
@@ -26,18 +32,26 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 	public final static String CONNECTOR_KIND = "trello";
 	public final static String REPOSITORY_URL = "https://trello.com";
 	public final static String REPOSITORY_LABEL = "Trello.com";
-	private final TrelloTaskDataHandler taskDataHandler = new TrelloTaskDataHandler();
+	private final TrelloTaskDataHandler m_taskDataHandler = new TrelloTaskDataHandler();
+	
+	private final String taskKeyUpdateDate = "UpdateDate";
 
 	@Override
 	public boolean canCreateNewTask(@NonNull TaskRepository a_repository)
 	{
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean canCreateTaskFromKey(@NonNull TaskRepository a_repository)
 	{
-		return false;
+		return true;
+	}
+	
+	@Override
+	public boolean canSynchronizeTask(TaskRepository taskRepository, ITask task) 
+	{
+		return true;
 	}
 
 	@Override
@@ -52,6 +66,12 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 		return REPOSITORY_LABEL;
 	}
 
+	@Override
+	public TrelloTaskDataHandler getTaskDataHandler() 
+	{
+		return m_taskDataHandler;
+	}
+	
 	@Override
 	public @Nullable String getRepositoryUrlFromTaskUrl(@NonNull String a_taskUrl)
 	{
@@ -92,22 +112,70 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 	@Override
 	public boolean hasTaskChanged(@NonNull TaskRepository a_taskRepository, @NonNull ITask a_task, @NonNull TaskData a_taskData)
 	{
-		return false;
+		TaskMapper mapper = (TaskMapper)getTaskMapping(a_taskData);
+		if (a_taskData.isPartial()) 
+		{
+			return mapper.hasChanges(a_task);
+		} 
+		else 
+		{
+			Date repositoryDate = mapper.getModificationDate();
+			Date localDate = new Date(Long.parseLong(a_task.getAttribute(taskKeyUpdateDate)));
+			if (repositoryDate != null && repositoryDate.equals(localDate)) 
+			{
+				return false;
+			}
+			return true;
+		}
 	}
 
 	@Override
 	public void updateTaskFromTaskData(@NonNull TaskRepository a_taskRepository, @NonNull ITask a_task, @NonNull TaskData a_taskData)
 	{
-		
+		TaskMapper mapper = (TaskMapper)getTaskMapping(a_taskData);
+		mapper.applyTo(a_task);
+		String status = mapper.getStatus();
+		if (status != null) 
+		{
+			if (mapper.getStatus().equals("closed")) 
+			{
+				Date modificationDate = mapper.getModificationDate();
+				if (modificationDate == null) 
+				{
+					modificationDate = new Date(0);
+				}
+				a_task.setCompletionDate(modificationDate);
+			} 
+			else 
+			{
+				a_task.setCompletionDate(null);
+			}
+		}
+		TrelloConnection client = new TrelloConnection(ITrelloConnection.DEFAULT_KEY, ITrelloConnection.DEFAULT_TOKEN);
+		try
+		{
+			a_task.setUrl(client.changeCard(a_task.getTaskId(), Card.NAME, client.getCardById(a_task.getTaskId()).getName()));
+		}
+		catch(IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*if (!a_taskData.isPartial()) 
+		{
+			a_task.setAttribute("SupportsSubtasks", Boolean.toString(taskDataHandler.supportsSubtasks(a_taskData)));
+			Date date = a_task.getModificationDate();
+			a_task.setAttribute("UpdateDate", (date != null) ? TracUtil.toTracTime(date) + "" : null); //$NON-NLS-1$
+		}*/
 	}
 
 	@Override
 	public @NonNull TaskData getTaskData(@NonNull TaskRepository a_repository, @NonNull String a_taskId, 
 			                             IProgressMonitor a_monitor) throws CoreException
 	{
-		return taskDataHandler.getTaskData(a_repository, a_taskId, a_monitor);
+		return m_taskDataHandler.getTaskData(a_repository, a_taskId, a_monitor);
 	}
-
+	
 	@Override
 	public IStatus performQuery(@NonNull TaskRepository a_repository, @NonNull IRepositoryQuery a_query, @NonNull TaskDataCollector a_collector, 
 			                    @Nullable ISynchronizationSession a_session, IProgressMonitor a_monitor)
@@ -140,9 +208,152 @@ public class TrelloRepositoryConnector extends AbstractRepositoryConnector
 			a_monitor.done();
 		}
 	}
+	
+	@Override
+	public void preSynchronization(ISynchronizationSession a_session, IProgressMonitor a_monitor) throws CoreException 
+	{
+		a_monitor = Policy.monitorFor(a_monitor);
+		try 
+		{
+			a_monitor.beginTask(Messages.TrelloRepositoryConnector_Getting_changed_tasks, IProgressMonitor.UNKNOWN);
 
+			if (!a_session.isFullSynchronization()) 
+			{
+				return;
+			}
+
+			if (a_session.getTasks().isEmpty()) 
+			{
+				return;
+			}
+
+			TaskRepository repository = a_session.getTaskRepository();
+
+			if (repository.getSynchronizationTimeStamp() == null || repository.getSynchronizationTimeStamp().length() == 0) 
+			{
+				for (ITask task : a_session.getTasks()) 
+				{
+					a_session.markStale(task);
+				}
+				return;
+			}
+
+			Date since = new Date(0);
+			try 
+			{
+				since = new Date(Integer.parseInt(repository.getSynchronizationTimeStamp()));
+			} 
+			catch (NumberFormatException e) 
+			{
+			}
+
+			try 
+			{
+				TrelloConnection client = new TrelloConnection(ITrelloConnection.DEFAULT_KEY, ITrelloConnection.DEFAULT_TOKEN);
+				Set<String> ids = client.getChangedCards(a_session.getTasks());
+			
+				System.err.println(" preSynchronization(): since=" + since.getTime() + ",changed=" + ids); //$NON-NLS-1$ //$NON-NLS-2$ 
+				
+				if (ids.isEmpty()) 
+				{
+					a_session.setNeedsPerformQueries(false);
+					return;
+				}
+
+				if (ids.size() == 1) 
+				{
+					System.err.println(" preSynchronization(): since=" + since.getTime());
+				}
+
+				for (ITask task : a_session.getTasks()) 
+				{
+					String id = task.getTaskId();
+					if (ids.contains(id)) 
+					{
+						a_session.markStale(task);
+					}
+				}
+			} catch (OperationCanceledException e) 
+			{
+				throw e;
+			} catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		} 
+		finally 
+		{
+			a_monitor.done();
+		}
+	}
+	
+	@Override
+	public void postSynchronization(ISynchronizationSession a_event, IProgressMonitor a_monitor) throws CoreException 
+	{
+		try 
+		{
+			a_monitor.beginTask("", 1);
+			if (a_event.isFullSynchronization() && a_event.getStatus() == null) 
+			{
+				Date date = getSynchronizationTimestamp(a_event);
+				if (date != null) 
+				{
+					a_event.getTaskRepository().setSynchronizationTimeStamp(Long.toString(date.getTime())); 
+				}
+			}
+		} 
+		finally 
+		{
+			a_monitor.done();
+		}
+	}
+
+	private Date getSynchronizationTimestamp(ISynchronizationSession a_event) 
+	{
+		Date mostRecent = new Date(0);
+		Date mostRecentTimeStamp = new Date (Integer.parseInt(a_event.getTaskRepository().getSynchronizationTimeStamp()));
+		for (ITask task : a_event.getChangedTasks()) 
+		{
+			Date taskModifiedDate = task.getModificationDate();
+			if (taskModifiedDate != null && taskModifiedDate.after(mostRecent)) 
+			{
+				mostRecent = taskModifiedDate;
+				mostRecentTimeStamp = task.getModificationDate();
+			}
+		}
+		return mostRecentTimeStamp;
+	}
+	
 	@Override
 	public void updateRepositoryConfiguration(@NonNull TaskRepository a_taskRepository, IProgressMonitor a_monitor) throws CoreException
 	{
+		
+	}
+	
+	@Override
+	public boolean canDeleteTask(TaskRepository a_repository, ITask a_task) 
+	{
+		return true;
+	}
+
+	@Override
+	public IStatus deleteTask(TaskRepository a_repository, ITask a_task, IProgressMonitor a_monitor) throws CoreException 
+	{
+		TrelloConnection client = new TrelloConnection(ITrelloConnection.DEFAULT_KEY, ITrelloConnection.DEFAULT_TOKEN);
+		try 
+		{
+			client.deleteCard(a_task.getTaskId());
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		return Status.OK_STATUS;
+	}
+	
+	@Override
+	public boolean canGetTaskHistory(TaskRepository a_repository, ITask a_task) 
+	{
+		return false;
 	}
 }
