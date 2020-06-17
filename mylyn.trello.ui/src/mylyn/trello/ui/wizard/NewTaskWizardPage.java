eff50870e0fd17ffc59c781c3e7259466ff00a15
дом <@169.254.170.118>
2020-06-17 23:45:37 +0300

    Добавлен класс мастера создания новой задачи.

---
+++ mylyn.trello.ui/src/mylyn/trello/ui/wizard/NewTaskWizardPage.java
@@ -0,0 +1,160 @@
+package mylyn.trello.ui.wizard;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class NewTaskWizardPage implements IWizardPage
+{
+
+	@Override
+	public void createControl(Composite a_parent)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Control getControl()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getErrorMessage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image getImage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMessage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void performHelp()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDescription(String a_description)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setImageDescriptor(ImageDescriptor a_image)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTitle(String a_title)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setVisible(boolean a_visible)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean canFlipToNextPage()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public IWizardPage getNextPage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWizardPage getPreviousPage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWizard getWizard()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPageComplete()
+	{
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void setPreviousPage(IWizardPage a_page)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setWizard(IWizard a_newWizard)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
