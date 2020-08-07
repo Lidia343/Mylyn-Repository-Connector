12e02f2f51c2319b4506199519f1b74e8feaeb09
Sorcerer <Sorcerer@LAPTOP-G4MS948M>
2020-08-07 17:52:34 +0300

    Создан интерфейс для проверки строкового значения. Его метод возвращает сообщение о результате проверки.

---
+++ mylyn.trello.ui/src/mylyn/trello/ui/validation/StringValueValidator.java
@@ -0,0 +1,10 @@
+package mylyn.trello.ui.validation;
+
+public interface StringValueValidator
+{
+	/**
+	 * @param a_value
+	 * @return сообщение о результате проверки
+	 */
+	String validate(String a_value);
+}
