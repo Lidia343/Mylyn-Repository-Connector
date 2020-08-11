e2698c978dc5fae003fd65008b132389dff72fb8
Sorcerer <Sorcerer@LAPTOP-G4MS948M>
2020-08-11 17:10:37 +0300

    В класс TrelloStringFieldEditor добавлено поле m_fillVertical для определения необходимости установки композиту и его текстовым элементам свойства изменять размер по горизонтали.

--- mylyn.trello.ui/src/mylyn/trello/ui/edit/TrelloStringFieldEditor.java
+++ mylyn.trello.ui/src/mylyn/trello/ui/edit/TrelloStringFieldEditor.java
@@ -4,15 +4,12 @@ import org.eclipse.core.runtime.Assert;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
-
-import mylyn.trello.ui.validation.StringValueValidator;
 
 public class TrelloStringFieldEditor
 {
@@ -22,59 +19,54 @@ public class TrelloStringFieldEditor
 	
 	private String m_labelText;
 	
-	private ModifyListener m_modifyListener;
+	private boolean m_fillVertical;
 	
-	private String m_errorMessage;
-	
-	public TrelloStringFieldEditor (@NonNull Composite a_parent, @NonNull String a_labelText)
+	public TrelloStringFieldEditor (@NonNull Composite a_parent, @NonNull String a_labelText, boolean a_fillVertical)
 	{
 		init (a_parent, a_labelText);
-		createControl();
+		m_fillVertical = a_fillVertical;
+		createControls();
 	}
 	
-	private void init (Composite a_parent, String a_labelText)
+	private void init (Composite a_parent, @NonNull String a_labelText)
 	{
-		//Assert.isNotNull(m_parent);
-		//Assert.isNotNull(m_labelText);
+		Assert.isNotNull(a_labelText);
 		
 		m_parent = a_parent;
 		m_labelText = a_labelText + " ";
 	}
 	
-	private void createControl ()
+	private void createControls ()
 	{
-		m_parent.setLayout(new GridLayout(2, false));
+		GridData data = new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL);
 		
-		GridData g = new GridData(GridData.FILL_HORIZONTAL);
-		m_parent.setLayoutData(g);
+		m_parent.setLayout(new GridLayout(2, false));
+		m_parent.setLayoutData(data);
 		
 		m_label = new Label(m_parent, SWT.NONE);
-		m_label.setText(m_labelText);
-		m_label.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+		m_label.setText(m_labelText + " ");
 		
-		m_text = new Text(m_parent, SWT.BORDER);
-		m_text.setLayoutData(g);
+		if (!m_fillVertical)
+		{
+			data.verticalAlignment = SWT.CENTER;
+			data.grabExcessVerticalSpace = false;
+			m_label.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+			m_text = new Text(m_parent, SWT.BORDER);
+		}
+		else
+		{
+			m_label.setLayoutData(new GridData(SWT.RIGHT, SWT.UP, false, false));
+			m_text = new Text(m_parent, SWT.BORDER | SWT.MULTI | SWT.WRAP);
+		}
+		m_text.setLayoutData(data);
 	}
 	
-	public void setStringValueValidator (@NonNull StringValueValidator a_validator)
+	public void setStringValueChangeListener (int a_eventType, @NonNull Listener a_listener)
 	{
-		//Assert.isNotNull(a_validator);
-		
-		if (m_modifyListener == null)
+		Assert.isNotNull(a_listener);
+		if (!m_text.isListening(a_eventType))
 		{
-			m_modifyListener = new ModifyListener ()
-			{
-				@Override
-				public void modifyText(ModifyEvent a_e)
-				{
-					m_errorMessage = a_validator.validate((String)a_e.data);
-				}
-			};
-		}
-		
-		if (!m_text.isListening(SWT.Modify))
-		{
-			m_text.addModifyListener(m_modifyListener);
+			m_text.addListener(a_eventType, a_listener);
 		}
 	}
 	
@@ -83,15 +75,20 @@ public class TrelloStringFieldEditor
 		return m_labelText;
 	}
 	
+	public void setEditable (boolean a_editable)
+	{
+		m_text.setEditable( a_editable);
+	}
+	
+	public void setStringValue (@NonNull String a_value)
+	{
+		Assert.isNotNull(a_value);
+		m_text.setText(a_value);
+	}
+	
 	@Nullable
 	public String getStringValue ()
 	{
 		return (m_text == null) ? null : m_text.getText();
-	}
-	
-	@Nullable
-	public String getErrorMessage ()
-	{
-		return m_errorMessage;
 	}
 }
