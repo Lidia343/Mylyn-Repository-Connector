8d1002773477753d37680e7b17130e19116594f2
Sorcerer <Sorcerer@LAPTOP-G4MS948M>
2020-08-13 12:03:11 +0300

    Из класса TrelloObject удалены ненужные методы.

--- mylyn.trello.core/src/trello/core/model/TrelloObject.java
+++ mylyn.trello.core/src/trello/core/model/TrelloObject.java
@@ -1,40 +1,18 @@
 package trello.core.model;
 
 /**
- * Класс представляет общую для объектов репозитория Trello информацию.
+ * Класс предоставляет общую для объектов репозитория Trello информацию.
  */
 public class TrelloObject
 {
 	protected String id;
 	protected String name;
 	
-	/**
-	 * @param a_id - id объекта
-	 */
-	public void setId(String a_id)
-	{
-		id = a_id;
-	}
-	
-	/**
-	 * @param a_name - имя объекта
-	 */
-	public void setName(String a_name)
-	{
-		name = a_name;
-	}
-	
-	/**
-	 * @return id объекта
-	 */
 	public String getId()
 	{
 		return id;
 	}
 	
-	/**
-	 * @return имя объекта
-	 */
 	public String getName()
 	{
 		return name;
